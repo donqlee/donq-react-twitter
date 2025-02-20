3936bff3b845066e904343ff8250ba2426b68db8
donqlee <donqlee88@gmail.com>
2025-02-21 00:20:08 +0900

    feat: Router components 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,7 @@
-import { Route, Routes, Navigate } from "react-router-dom";
+import Router from "components/Router";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<h1>Home Page</h1>}></Route>
-      <Route path="/posts" element={<h1>Post List Page</h1>}></Route>
-      <Route path="/posts/:id" element={<h1>Post Detail Page</h1>}></Route>
-      <Route path="/posts/new" element={<h1>Post New Page</h1>}></Route>
-      <Route path="/posts/edit/:id" element={<h1>Post Edit Page</h1>}></Route>
-      <Route path="/profile" element={<h1>Profile Page</h1>}></Route>
-      <Route path="/profile/edit" element={<h1>Profile Edit Page</h1>}></Route>
-      <Route
-        path="/notifications"
-        element={<h1>Notifications Page</h1>}
-      ></Route>
-      <Route path="/search" element={<h1>Search Page</h1>}></Route>
-      <Route path="/user/login" element={<h1>Login Page</h1>}></Route>
-      <Route path="/user/signup" element={<h1>Signup Page</h1>}></Route>
-      <Route path="*" element={<Navigate replace to="/" />}></Route>
-    </Routes>
-  );
+  return <Router />;
 }
 
 export default App;
